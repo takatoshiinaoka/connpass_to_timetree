c53237bbf77d65267b20d633275a4f11c5c0715e
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-10-28 22:33:56 +0900

    commit

--- env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo TIMETREE_TOKEN=${{ secrets.TIMETREE_TOKEN }}"\n"\
-TIMETREE_BASEURL=${{ secrets.TIMETREE_BASEURL }}"\n"\
-CALENDAR_ID=${{ secrets.CALENDAR_ID }}"\n"\
-NICKNAME=${{ secrets.NICKNAME }} >> .env
