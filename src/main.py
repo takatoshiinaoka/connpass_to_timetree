acb53238644eb94c554fd5e13c433ea0bd12b500
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-10-28 21:47:01 +0900

    commit

--- src/main.py
+++ src/main.py
@@ -8,20 +8,17 @@ TOKEN = os.environ.get('TIMETREE_TOKEN')
 TIMETREE_BASEURL = os.environ.get('TIMETREE_BASEURL')
 CALENDAR_ID = os.environ.get('CALENDAR_ID')
 
-# Json読み込み
+# JsonからTimeTreeに登録済みのイベントID読み込み
 json_open = open('./events.json', 'r')
-json = json.load(json_open)
+jsonArray = json.load(json_open)
 
 # Connpassからイベント取得
 nickname = os.environ.get('NICKNAME')
 responce = requests.get(f"https://connpass.com/api/v1/event/?count=100&nickname={nickname}").json()
 events = responce["events"]
 
-# JsonのイベントIDとConnpassのイベントIDを取得
-jsonArray = []
+# ConnpassのイベントIDを取得
 connpassArray = []
-for event in json:
-    jsonArray.append(event["event_id"])
 for event in events:
     connpassArray.append(event["event_id"])
 
@@ -31,7 +28,7 @@ diff_id = set(connpassArray) ^ set(jsonArray)
 
 # TimeTreeに登録する関数
 def post_timetree(event):
-    # print("Hello",event["event_id"])
+    print("Hello",event["event_id"])
     headers = {'Authorization': f"Bearer {TOKEN}"}
     data = {
         "data": {
@@ -66,11 +63,9 @@ for id in diff_id:
         if(event["event_id"] == id):
             post_timetree(event)
 
-
-
-
-
-
-
-
+# TimeTreeに登録したイベントIDの情報を更新
+path = './events.json'
+json_file = open(path, mode="w")
+json.dump(connpassArray, json_file, ensure_ascii=False)
+json_file.close()
 
